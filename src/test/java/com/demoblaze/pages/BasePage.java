fa4aa8efefcb00482ee0eee678637c47c28e4524
Sevki <sevki.oksuz@gmail.com>
2022-02-15 16:02:54 +0100

    BrowserUtils added

--- src/test/java/com/demoblaze/pages/BasePage.java
+++ src/test/java/com/demoblaze/pages/BasePage.java
@@ -1,4 +1,22 @@
 package com.demoblaze.pages;
+import com.demoblaze.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
-public class BasePage {
+import java.util.List;
+
+public abstract class BasePage {
+    public BasePage(){
+        PageFactory.initElements(Driver.get(),this);
+    }
+
+
+    @FindBy(xpath = "//a[contains(.,'Home')]")
+    public WebElement home;
+    @FindBy(xpath = "//a[.='Cart']")
+    public WebElement cart;
+
+    @FindBy(css = ".hrefch")
+    public List<WebElement> products;
 }
---
+++ src/test/java/com/demoblaze/utilities/BrowserUtils.java
@@ -0,0 +1,35 @@
+package com.demoblaze.utilities;
+
+import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BrowserUtils {
+
+    public static void sleep(int second) {
+
+        second *= 1000;
+
+        try {
+
+            Thread.sleep(second);
+
+        } catch (InterruptedException e) {
+
+            System.out.println("something happened in the sleep method");
+
+        }
+
+    }
+
+    public static List<String> getElementsText(List<WebElement> list){
+        List<String> elemTexts = new ArrayList<>();
+        for (WebElement webElement : list) {
+            elemTexts.add(webElement.getText());
+        }
+        return elemTexts;
+    }
+
+
+}
